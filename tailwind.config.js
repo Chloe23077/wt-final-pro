9a60a3013926000951fbf75d0393bb6727589ac3
unknown <yunjasgu@gmail.com>
2024-06-07 15:09:19 +0800

    feat(home page): add home page html code

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './*.html',
+      "./src/**/*.{html,js}",
+      "./assets/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
